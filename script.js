ac9018d83c30b32240e2c1031e3c5feedd546245
SnehaFSD <sinegashaan@gmail.com>
2023-09-15 22:30:50 +0530

    updated

--- script.js
+++ script.js
@@ -2,37 +2,43 @@
 let currentPlayer = 1;
     let player1Score = 0;
     let player2Score = 0;
-    
+
     function startGame() {
-        document.getElementById('rollButton')= false;
-        document.getElementById('resetButton') = true;
-        currentPlayer = Math.floor(Math.random() * 2) + 1;
+      document.getElementById('rollButton').disabled = false;
+      document.getElementById('resetButton').disabled = true;
+      currentPlayer = Math.floor(Math.random() * 2) + 1;
+      alert(`Player ${currentPlayer} starts the game.`);
+    }
+
+    function rollDice() {
+      const diceValue = Math.floor(Math.random() * 6) + 1;
+      alert(`Player ${currentPlayer} rolled a ${diceValue}.`);
+
+      if (currentPlayer === 1) {
+        player1Score += diceValue;
+        document.getElementById('player1Score').textContent = player1Score;
+      } else {
+        player2Score += diceValue;
+        document.getElementById('player2Score').textContent = player2Score;
       }
-  
-      function rollDice() {
-        const diceValue = Math.floor(Math.random() * 6) + 1;
-  
-        if (currentPlayer === 1) {
-          player1Score += diceValue;
-          document.getElementById('player1Score').textContent = player1Score;
-        } else {
-          player2Score += diceValue;
-          document.getElementById('player2Score').textContent = player2Score;
-        }
-  
-        if (player1Score >= 30 || player2Score >= 30) {
-          document.getElementById('rollButton') = true;
-          document.getElementById('resetButton')= false;
-        } else {
-          currentPlayer = currentPlayer === 1;
-        }
+
+      if (player1Score >= 30 || player2Score >= 30) {
+        alert(`Player ${currentPlayer} wins!`);
+        document.getElementById('rollButton').disabled = true;
+        document.getElementById('resetButton').disabled = false;
+      } else {
+        currentPlayer = currentPlayer === 1 ? 2 : 1;
+        alert(`Player ${currentPlayer}'s turn.`);
       }
-  
-      function resetGame() {
-        currentPlayer = 1;
-        player1Score = 0;
-        player2Score = 0;
-        document.getElementById('player1Score') = player1Score;
-        document.getElementById('player2Score') = player2Score;
-        alert('Game reset.');
-      }
+    }
+
+    function resetGame() {
+      currentPlayer = 1;
+      player1Score = 0;
+      player2Score = 0;
+      document.getElementById('player1Score').textContent = player1Score;
+      document.getElementById('player2Score').textContent = player2Score;
+      document.getElementById('rollButton').disabled = false;
+      document.getElementById('resetButton').disabled = true;
+      alert('Game has been reset.');
+    }
